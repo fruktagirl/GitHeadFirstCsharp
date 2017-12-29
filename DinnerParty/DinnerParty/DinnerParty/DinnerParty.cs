1cbb0fe85bb54efde9db0858037b54a8479c273d
Emese Vincze-Jancsi <emese.vincze-jancsi@tss-yonder.com>
2017-12-29 15:24:21 +0200

    Created form and added DinnerParty class

---
+++ DinnerParty/DinnerParty/DinnerParty/DinnerParty.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DinnerParty
+{
+    class DinnerParty
+    {
+
+        public int NumberOfPeople;
+        public decimal CostOfBeverages;
+        public decimal CostOfDecorations;
+
+        public bool HealthyOption;
+        public decimal TotalCost;
+
+        public const decimal fancyDecorationPerPersonFancy = 15M;
+        public const decimal fancyDecorationPerPersonNotFancy = 1.5M;
+        public const decimal fancyFee = 50M;
+        public const decimal notfancyFee = 30M;
+
+
+        public void SetHealthyOption(bool healthyOption)
+        {
+            //uses the value of variable healthyOption to update the CostOfBeveragesPerPerson field based on the healthyOption value
+            HealthyOption = healthyOption;
+        }
+
+        public decimal CalculateCostOfDecorations(int numberOfPeople, bool fancyDecoration)
+        {
+            //cost of decorations for the number of people attending the party
+
+            if(fancyDecoration == true)
+            {
+                CostOfDecorations = (numberOfPeople * fancyDecorationPerPersonFancy) + fancyFee;
+            }
+            else
+            {
+                CostOfDecorations = (numberOfPeople * fancyDecorationPerPersonNotFancy) + notfancyFee;
+            }
+       
+            return CostOfDecorations;
+        }
+
+        public decimal CalculateCost()
+        {
+            //calculates total cost by adding the cost of decorations to the cost of the drinks per person
+            //if the client wants the Healthy option, apply the discount inside the CalculateCost() method
+
+            TotalCost = CostOfBeverages + CostOfDecorations;
+
+            if(HealthyOption == true)
+            {
+                TotalCost = TotalCost - (TotalCost * 5 / 100);
+            }
+
+            return TotalCost;
+        }
+    }
+}
---
+++ DinnerParty/DinnerParty/DinnerParty/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DinnerParty
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+        private void DisplayDinnerPartyCost()
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            dinnerParty.HealthyOption = checkBox2.Checked;
+            bool fancyDecoration = checkBox1.Checked;
+            decimal totalCost = dinnerParty.CalculateCost();
+
+            textBox1.Text = totalCost.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinnerParty = new DinnerParty();
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+    }
+}
