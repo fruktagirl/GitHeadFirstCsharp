d2e915c44dd9ab9f25f335afc7e67d309fa0e51d
Emese Vincze-Jancsi <emese.vincze-jancsi@tss-yonder.com>
2017-12-29 12:12:02 +0200

    Mileage calculator

---
+++ Calculator/Calculator/Calculator/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        int startingMileage;
+        int endingMileage;
+        double milesTraveled;
+        double reimburseRate = 0.39;
+        double amountOwed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = Convert.ToInt32(numericUpDown1.Value);
+            endingMileage = Convert.ToInt32(numericUpDown2.Value);
+
+            //verify starting and ending mileage
+            if(startingMileage > endingMileage)
+            {
+                MessageBox.Show("The starting mileage must be less than the ending mileage.", "Cannot Calculate");
+            }
+            else
+            {
+                //calculate milesTraveled
+                milesTraveled = endingMileage - startingMileage;
+
+                //calculate amountOwed
+                amountOwed = milesTraveled * reimburseRate;
+
+                label4.Text = "$" + amountOwed;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show( milesTraveled + " miles", "Miles Traveled");
+        }
+    }
+}
